815cc0b5982dd30ea867d542f9b1aca3220b89d6
irfiacre <irfiacre@gmail.com>
2025-12-30 14:17:59 +0200

    Add initial files

---
+++ utils/data_cleanup.py
@@ -0,0 +1,10 @@
+# Used to make a dataset that is compatible with the model "DeepSeek-R1-Distill-Qwen-7B".
+
+"""
+Approach:
+
+0. Understand the template to pass to the model.
+0.1 Understand how to create HuggingFace datasets.
+1. Process the PDF file.
+2. Finalize the dataset & Push it to hugging face.
+"""
