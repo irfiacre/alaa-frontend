98db8656eb9d79fe2b28c058e4cdf9a25438642d
irfiacre <irfiacre@gmail.com>
2026-01-13 22:40:35 +0200

    Improve modal deployment

--- modal/test.py
+++ modal/test.py
@@ -1,7 +1,18 @@
-import modal
+# import modal
 
-LawDeepSeekModel = modal.Cls.from_name("law-deepseek-r1", "LawDeepSeekModel")
+# LawDeepSeekModel = modal.Cls.from_name("law-deepseek-r1", "LawDeepSeekModel")
 
-model = LawDeepSeekModel()
-result = model.generate.remote("Explain to me article 170?")
-print(result)
+# model = LawDeepSeekModel()
+# result = model.generate.remote("Explain to me article 170?")
+# print(result)
+
+import requests
+
+response = requests.post(
+    "https://irfiacre-dev--law-deepseek-r1-lawdeepseekmodel-web-generate.modal.run",
+    json={
+        "prompt": "What is the article 152?",
+    }
+)
+
+print(response.text)
